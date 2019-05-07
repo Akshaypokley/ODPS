3d3fb2277fe7a2db2f543a3fe520886f48bb7e78
akshay.pokley <akshay85pokley@gmail.com>
2019-05-07 17:31:16 +0530

    f

---
+++ src/main/java/com/bpms/ODPS/App.java
@@ -0,0 +1,17 @@
+package com.bpms.ODPS;
+
+
+import org.testng.annotations.Test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    @Test
+    public void demoTest()
+    {
+        System.out.println( "Hello World!" );
+    }
+}
